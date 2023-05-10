581a10602620331efd7bbfdfa3f41e1fd0af35c0
SebastianV99 <vollmer.sebastian@outlook.de>
2023-05-10 11:35:30 +0200

    vergleich wien

---
+++ main.js
@@ -0,0 +1,74 @@
+/* Wetterstationen Euregio Beispiel */
+
+// Innsbruck
+let ibk = {
+    lat: 47.267222,
+    lng: 11.392778
+};
+
+// Karte initialisieren
+let map = L.map("map", {
+    fullscreenControl: true
+}).setView([ibk.lat, ibk.lng], 11);
+
+// thematische Layer
+let themaLayer = {
+    stations: L.featureGroup().addTo(map),
+}
+
+// Hintergrundlayer
+let layerControl = L.control.layers({
+    "Relief avalanche.report": L.tileLayer(
+        "https://static.avalanche.report/tms/{z}/{x}/{y}.webp", {
+        attribution: `© <a href="https://lawinen.report">CC BY avalanche.report</a>`
+    }).addTo(map),
+    "Openstreetmap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
+    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery")
+}, {
+    "Wetterstationen": themaLayer.stations.addTo(map)
+}).addTo(map);
+
+// Maßstab
+L.control.scale({
+    imperial: false,
+}).addTo(map);
+
+// Vienna Sightseeing Haltestellen ?? Wetterstationen
+async function showStations(url) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+    L.geoJSON(jsondata, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: `icons/icons.png`,
+                    //iconSize: [32, 37],
+                    iconAnchor: [16, 37], 
+                    popupAnchor: [0, -37],
+                })
+            });
+        },
+        onEachFeature: function (feature, layer) {
+            let prop = feature.properties;
+            layer.bindPopup(`
+        <h4> ${prop.name} </h4> <br>
+        <h5> </h5>
+        <ul>
+        ${prop.LT} 
+        ${prop.RH}
+        ${prop.WG}
+        ${prop.HS}
+        </ul>
+            `);
+             console.log(prop)
+        }
+    }).addTo(themaLayer.stops);
+    console.log(response, jsondata)
+
+}
+
+    // Wetterstationen mit Icons und Popups implementieren
+
+
+showStations("https://static.avalanche.report/weather_stations/stations.geojson");
